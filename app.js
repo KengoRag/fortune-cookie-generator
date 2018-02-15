6e5709a2c8ef26e6deb482d4dae4f3a7b4f5cb7b
KengoRag <kengoraganit@gmail.com>
2018-02-14 14:27:55 -1000

    ?

---
+++ app.js
@@ -0,0 +1,15 @@
+var fortunes = [
+	"Slip off a cliff and die",
+	"Get zapped by lightning and die",
+	"Trip into an open man hole and die",
+	"Get kicked by a horse and die",
+	"Accidentally walk into the lion enclosure and die",
+	"Win the lottery then immediately have a heart attack and die",
+	"Discover the secret to immortality but die",
+	"A random flying baseball hits you in the head and you die",
+	"Find a dollar bill on the floor"
+];
+
+var randomFortune = Math.floor(Math.random()*fortunes.length);
+
+console.log(fortunes[randomFortune]);
